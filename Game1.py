e6ce1d04dbb34893491120cc403ab89e20994547
Shwetha Shree Satish <73294066+shwetha-shree-s@users.noreply.github.com>
2023-08-02 13:52:19 -0400

    R-P-S_game.py

--- Game1.py
+++ Game1.py
@@ -1,6 +1,6 @@
 import random
 def get_choices():
-    player_choice = input("Enter the choice (rock, paper, scissors:)")
+    player_choice = input("Enter your choice (rock, paper, scissors:)")
     options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choices = {"player":player_choice,"computer":computer_choice}
